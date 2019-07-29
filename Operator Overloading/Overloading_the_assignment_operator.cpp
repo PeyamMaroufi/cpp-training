64dc5d1a3e49c980bffbf6d686e6652f85499b90
Peyam M <maroufi@debian>
2019-07-29 19:56:38 +0200

    Removed compiled files

--- Operator Overloading/Overloading_the_assignment_operator.cpp
+++ Operator Overloading/Overloading_the_assignment_operator.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
+class Test
+{
+    int id;
+    string name;
+
+public:
+    Test() : id(0), name("") {}
+    Test(int id, string name) : id(id), name(name) {}
+    void print()
+    {
+        cout << id << ":" << name << endl;
+    }
+};
+
 int main()
 {
+    Test test1(10, "Mike");
+    test1.print();
+
+    Test test2(20, "Bob");
+
     return 0;
 }
