ba57e3989297d6ca07245f3410590fb516ec7578
Peyam M <maroufi@debian>
2019-08-13 19:28:37 +0200

    Virtual function and what constructor to call

--- Passing Functions to Functions/Object Slicing and Polymorphism/main.cpp
+++ Passing Functions to Functions/Object Slicing and Polymorphism/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 using namespace std;
-int main(){
+class Parent
+{
+public:
+	Parent() {}
+	Parent(const Parent &other) { cout << "Copy constructor " << endl; }
+	/* Without the virtual keyword we could not call the correct function as in line 27-31  */
+	virtual void print()
+	{
+		cout << "parent" << endl;
+	}
+	virtual ~Parent() {}
+};
+
+/* Creating a child class to the parent */
+class Child : public Parent
+{
+public:
+	/* For this to work we need to have a default constructor in the parent class. */
+	void print()
+	{
+		cout << "child" << endl;
+	}
+};
+
+int main()
+{
+	Child c1;
+	/* The lesson is that when we refere a superclass to a subclass we can get the subclass's
+	 * functions and methods. */
+	Parent &p1 = c1;
+	p1.print();
+
+	/* In this with equal sign we actually pass in the Child to the copy constructor. But if we
+     *  have a copy constructor then we have to have a default constructor in order to the child
+     * class to work */
+	Parent p2 = Child();
+	p2.print();
 	return 0;
 }
