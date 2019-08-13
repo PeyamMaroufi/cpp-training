ddeab3836541800abc3bd0e6be693b08e9872731
Peyam M <maroufi@debian>
2019-08-13 20:07:09 +0200

    Functors simply explaind

--- Passing Functions to Functions/Functors/main.cpp
+++ Passing Functions to Functions/Functors/main.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+/*  We can also use this as a class and let the next struc derive from it */
+struct Test
+{
+	virtual bool operator()(string &test) = 0;
+	virtual ~Test() {}
+};
+
+/* Lets use a struct because all the memebers are by default public. in class the members are
+ * by default private. It is actually a "class" with public members and functions.*/
+struct MatchTest : public Test
+{
+	/* we are overloading the () */
+	bool operator()(string &text)
+	{
+		return text == "lion";
+	}
+};
+
+void check(string text, Test &test)
+{
+	if (test(text))
+	{
+		cout << "Text matches" << endl;
+	}
+	else
+	{
+		cout << "No match" << endl;
+	}
+}
+int main()
+{
+	MatchTest pred;
+	/* Since we have a reference as in argument then we use a separate variable for the string. */
+	string value = "lion";
+	/* So here we are actually doing the () operator */
+	cout << pred(value) << endl;
+
+	MatchTest m;
+	check("lion", m);
 	return 0;
 }
