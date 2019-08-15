84945ffe665dca7a61276789083735e1769e145b
Peyam M <maroufi@debian>
2019-08-15 20:17:32 +0200

    auto keyword in functions and variables

--- C++11 New stuff/AutoKey/main.cpp
+++ C++11 New stuff/AutoKey/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
+
+/* Auto function returning an instance type. The type can be identified using decltype.  */
+template <class T, class S>
+auto test(T value, S value2) -> decltype(value + value2)
+{
+	return value + value2;
+}
+
+auto get() -> int
+{
+	return 999;
+}
+
+auto test2() -> decltype(get())
+{
+	return get();
+}
+
 int main()
 {
+	/*  A more C# like thing. in C# you write var . in here it does it with auto keyword */
+	auto value = 7;
+	auto text = "Hello";
+
+	cout << value << endl;
+	cout << text << endl;
+	cout << test(5, 6) << endl;
+
+	cout << test2() << endl;
 	return 0;
 }
--- C++11 New stuff/Typeid and Name Mangling/main.cpp
+++ C++11 New stuff/Typeid and Name Mangling/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
+
 int main()
 {
+	string value;
+
+	/* C++ specific */
+	decltype(value) name = "Payam";
+
+	/*  Shows the typ of the variable  */
+	cout << typeid(value).name() << endl;
+	cout << name << endl;
+
 	return 0;
 }
